1f53404319798d912e3cdb592211e59c9abf15a0
Yhonniel Suárez <yhonnielsuarez@gmail.com>
2022-06-05 18:17:58 -0500

    :D

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -12,7 +12,8 @@ export const getLinks = async (req, res) => {
   }
 };
 
-export const getLink = async (req, res) => {
+//PARA UN CRUD TRADICIONAL
+export const getLinkCRUD = async (req, res) => {
   try {
     const { id } = req.params;
     const link = await Link.findById(id);
@@ -24,6 +25,23 @@ export const getLink = async (req, res) => {
       return res.status(401).json({ error: "No te pertenece ese id 🤡 " });
 
     return res.json({ link });
+  } catch (error) {
+    console.log(error);
+    if (error.kind === "ObjectId") {
+      return res.status(403).json({ error: "Formato id Incorrecto" });
+    }
+    return res.status(500).json({ error: "error de servidor" });
+  }
+};
+
+export const getLink = async (req, res) => {
+  try {
+    const { nanoLink } = req.params;
+    const link = await Link.findOne({ nanoLink });
+
+    if (!link) return res.status(404).json({ error: "No existe el link" });
+
+    return res.json({ longLink: link.longLink });
   } catch (error) {
     console.log(error);
     if (error.kind === "ObjectId") {
@@ -62,6 +80,40 @@ export const removeLink = async (req, res) => {
       return res.status(401).json({ error: "No te pertenece ese id 🤡 " });
 
     await link.remove();
+
+    return res.json({ link });
+  } catch (error) {
+    console.log(error);
+    if (error.kind === "ObjectId") {
+      return res.status(403).json({ error: "Formato id Incorrecto" });
+    }
+    return res.status(500).json({ error: "error de servidor" });
+  }
+};
+
+export const updateLink = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { longLink } = req.body;
+
+    console.log(longLink);
+
+    if (!longLink.startsWith("https://")) {
+      longLink = "https://" + longLink;
+    }
+
+    const link = await Link.findById(id);
+
+    if (!link) return res.status(404).json({ error: "No existe el link" });
+
+    //no publico
+    if (!link.uid.equals(req.uid))
+      return res.status(401).json({ error: "No te pertenece ese id 🤡 " });
+
+    // actualiza
+
+    link.longLink = longLink;
+    await link.save({ longLink });
 
     return res.json({ link });
   } catch (error) {
--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 import "dotenv/config";
 import "./database/connectdb.js";
 import express from "express";
+import cors from "cors";
+
 import authRouther from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import linkRouter from "./routes/link.route.js";
+import redirectRouter from "./routes/redirect.route.js";
+
 const app = express();
+
+const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("Error de CORS origin: " + origin + " No autorizado!");
+    },
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
+
+// ejemlo back redirect opcional
+app.use("/", redirectRouter);
+
 app.use("/api/v1/auth", authRouther);
 app.use("/api/v1/links", linkRouter);
 // solo para el ejemplo del login/tojken
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3001;
 
